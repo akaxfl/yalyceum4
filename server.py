852ecafcc34babb2e48132c2e0ad5dc17e21a567
akaxfl <161625156+akaxfl@users.noreply.github.com>
2024-04-02 16:33:29 +0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,42 @@
+﻿from flask import Flask, render_template
+from flask_restful import Api
+from flask_login import LoginManager
+
+from data import db_session, users_resources, jobs_resources
+from data.users import User
+from data.jobs import Jobs
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+api = Api(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Jobs).all()
+    users = db_sess.query(User).all()
+    names = {name.id: (name.surname, name.name) for name in users}
+    return render_template("index.html", jobs=jobs, names=names, title='Work log')
+
+
+def main():
+    db_session.global_init("db/mars_explorer.sql")
+    api.add_resource(users_resources.UsersListResource, '/api/v2/users')
+    api.add_resource(users_resources.UsersResource, '/api/v2/users/<int:user_id>')
+    api.add_resource(jobs_resources.JobsListResource, '/api/v2/jobs')
+    api.add_resource(jobs_resources.JobsResource, '/api/v2/jobs/<int:job_id>')
+    app.run()
+
+
+if __name__ == '__main__':
+    main()
